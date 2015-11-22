e277530c38ba0002c140dae49298e6fe71964302
Jason Carter <jasonwcarter@outlook.com>
2015-11-21 20:41:10 -0700

    Playing around with having a bot return data from an api.

--- slack-mentorbot/commands/calculate.rb
+++ slack-mentorbot/commands/calculate.rb
@@ -6,8 +6,17 @@ module SlackMentorbot
       end
 
       command 'insult' do |client, data, _match|
-        
         send_message client, data.channel, 'Screw you, hippy!'
+      end
+
+      command 'mentors' do |client, data, _match|
+        response = HTTParty.get('http://localhost:3000/mentors.json')
+        mentors = response.parsed_response
+
+        send_message client, data.channel, "Here are the list of mentors: "
+        mentors.each do |mentor|
+          send_message client, data.channel, "#{mentor['name']}"
+        end
       end
     end
   end
